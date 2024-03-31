2dc7de42bfb260aecd0cab642ad3ae91ef26eef0
vikram10-6 <vikramayer19@gmail.com>
2024-04-01 03:36:05 +0530

    Added all basic folder structures with also a headstart on sidebar functionality

---
+++ constants/index.ts
@@ -0,0 +1,28 @@
+export const sidebarLinks = [
+    {
+        label:'Home',
+        route:'/',
+        imgUrl:'icons/Home.svg',
+    },
+    {
+        label:'Upcoming',
+        route:'/Upcoming',
+        imgUrl:'icons/Upcoming.svg',
+    },
+    {
+        label:'Previous',
+        route:'/Previous',
+        imgUrl:'icons/Previous.svg',
+    },
+    {
+        label:'Recordings',
+        route:'/Recordings',
+        imgUrl:'icons/Video.svg',
+    },
+    {
+        label:'Personal Room',
+        route:'/Personal-room',
+        imgUrl:'icons/add-personal.svg',
+    },
+    
+]
